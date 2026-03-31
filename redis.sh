887f3b8d8eafad816dcbf5b4fc2c0507d8092fa9
mepavankumar15 <119803977+mepavankumar15@users.noreply.github.com>
2026-03-31 15:25:42 +0530

    actions

--- redis.sh
+++ redis.sh
@@ -32,7 +32,7 @@ VALIDATE $? "Enable redis "
 dnf install redis -y &>>LOGS_FILE
 VALIDATE $? "Redis Installation"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 'protected-mode/ c \protected-mode no' /etc/redis/redis.conf &>>LOGS_FILE
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>LOGS_FILE
 VALIDATE $? "The configuration of redis "
 
 systemctl enable redis &>>LOGS_FILE
