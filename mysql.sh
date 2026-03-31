0c2069e3cf0284e18d967720d90ebc2254db7178
mepavankumar15 <119803977+mepavankumar15@users.noreply.github.com>
2026-03-31 15:58:09 +0530

    actions

---
+++ mysql.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER='/var/log/shell-roboshop'
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+
+if [ $USER_ID -ne 0 ]; then
+    echo -e " $R the user is not in root path $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 is .. $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else 
+        echo -e "$2 is .. $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install mysql-server -y
+VALIDATE $? "MYSQL Server installation"
+
+systemctl enable mysqld
+systemctl start mysqld  
+VALIDATE $? "Start and enabling system "
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Password initiation "
