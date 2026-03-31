db9b1d6056151a0cda479c84032dbcf33d00a34f
mepavankumar15 <119803977+mepavankumar15@users.noreply.github.com>
2026-03-31 11:12:43 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -39,7 +39,7 @@ dnf install nodejs -y &>>$LOGS_FILE
 VALIDATE $? "Installation of nodejs"
 
 id roboshop &>>$LOGS_FILE
-if [$? -ne 0]; then
+if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
     VALIDATE $? "Adding application user"
 else
